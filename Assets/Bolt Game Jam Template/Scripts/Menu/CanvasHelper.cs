a96377450ea1aa733eef1c62048cfdf76725f5bb
vuongkm <vuongkm01526@gmail.com>
2021-01-26 15:37:46 +0700

    Initial Commit

---
+++ Assets/Bolt Game Jam Template/Scripts/Menu/CanvasHelper.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class CanvasHelper : MonoBehaviour {
+    public static IEnumerator FadeCanvasGroupAlpha(float startAlpha, float endAlpha, float duration, CanvasGroup canvasGroupToFadeAlpha)
+    {
+        float elapsedTime = 0f;
+        while (elapsedTime < duration)
+        {
+            elapsedTime += Time.deltaTime;
+            float currentAlpha = Mathf.Lerp(startAlpha, endAlpha, elapsedTime / duration);
+            canvasGroupToFadeAlpha.alpha = currentAlpha;
+            yield return null;
+        }
+    }
+}
